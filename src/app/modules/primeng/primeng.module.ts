0525e9d41c3dc57e29791ebb3f9a8d5f23802c1a
Cartaxo1 <cartaxojoao1@gmail.com>
2024-10-01 18:20:05 -0300

    cria tabela renderiza produtos

--- src/app/modules/primeng/primeng.module.ts
+++ src/app/modules/primeng/primeng.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonModule],
-  exports: [ButtonModule],
+  imports: [CommonModule, ButtonModule, TableModule],
+  exports: [ButtonModule, TableModule],
 })
 export class PrimengModule {}
